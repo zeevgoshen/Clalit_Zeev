4e01c34955ab21ec82ff62797ab3032e31a80701
Ze'ev Goshen <zeevgoshen@gmail.com>
2023-05-26 23:47:20 +0300

    - removed mobile support files - using state loading true/false to display "Loading" paragraph

--- Clalit_Zeev/ClientApp/src/components/ExchangeRates.js
+++ Clalit_Zeev/ClientApp/src/components/ExchangeRates.js
@@ -1,4 +1,4 @@
-﻿import React, {  useState, useEffect } from "react";
+﻿import React, { useState, useEffect } from "react";
 import ExchangeService from "../services/exchange_rates.service.js";
 import { NO_RESULTS } from "../constants/messages.js"
 import "./ExchangeRates.css";
@@ -14,30 +14,32 @@ export default function ExchangeRates() {
     }, []);
 
     return (
-        <table className='table table-striped' aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Current Change</th>
-                    <th>Current Exchange Rate</th>
-                    <th>Key</th>
-                    <th>Last Update</th>
-                    <th>Unit</th>
-                </tr>
-            </thead>
-            <tbody>
-                {exchangerates.response?.length === 0 ?
-                    (<tr><td colSpan="5">{NO_RESULTS}</td></tr>) :
-                    (exchangerates.response?.map(exchangerate =>
-                        <tr key={exchangerate.key}>
-                            <td>{exchangerate.currentChange}</td>
-                            <td>{exchangerate.currentExchangeRate}</td>
-                            <td>{exchangerate.key}</td>
-                            <td>{exchangerate.lastUpdate}</td>
-                            <td>{exchangerate.unit}</td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
+        exchangerates.loading ?
+            (<p><em>Loading...</em></p>) :
+            (<table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Current Change</th>
+                        <th>Current Exchange Rate</th>
+                        <th>Key</th>
+                        <th>Last Update</th>
+                        <th>Unit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {exchangerates.response?.length === 0 ?
+                        (<tr><td colSpan="5">{NO_RESULTS}</td></tr>) :
+                        (exchangerates.response?.map(exchangerate =>
+                            <tr key={exchangerate.key}>
+                                <td>{exchangerate.currentChange}</td>
+                                <td>{exchangerate.currentExchangeRate}</td>
+                                <td>{exchangerate.key}</td>
+                                <td>{exchangerate.lastUpdate}</td>
+                                <td>{exchangerate.unit}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>)
     );
 }
